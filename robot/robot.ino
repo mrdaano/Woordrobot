c9ff1d805854a1f3682c5dcc6caa660255d48f48
Daan Kloetstra <daankloetstra@hotmail.com>
2016-03-16 13:12:39 +0100

    Links en rechts

--- robot/robot.ino
+++ robot/robot.ino
@@ -2,6 +2,7 @@ int M1 = 4;
 int M1b = 5;
 int M2b = 6;
 int M2 = 7;
+int letter = 1;
 void setup() {
   // put your setup code here, to run once:
   pinMode(M1, OUTPUT);
@@ -10,7 +11,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  vooruit(1000);
+  achteruit(1000);
   delay(1000);
 }
 
@@ -33,8 +34,44 @@ void vooruit(int tijd) {
   delay(tijd);
   stil();
  }
+ 
+ void links(int tijd) {
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, LOW);
+  analogWrite(M1b, 255);
+  analogWrite(M2b, 255);
+  delay(tijd);
+  stil();
+ }
+
+ void rechts(int tijd) {
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, HIGH);
+  analogWrite(M1b, 255);
+  analogWrite(M2b, 255);
+  delay(tijd);
+  stil();
+ }
+ 
 
  void stil() {
   analogWrite(M1b, 0);
   analogWrite(M2b, 0);
  }
+
+ // Woord
+
+void schrijfWoord() {
+ 
+}
+
+ void l() {}
+
+ void i() {}
+
+ void e() {}
+
+ void f() {}
+
+
+// Display
